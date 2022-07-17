2faac1c2e06bc3b9e9f517f51b99e76ca86626b1
mohamed1010 <workmail1010@gmail.com>
2022-07-17 17:12:47 +0200

    cam2

--- src/app/pages/camera/camera-routing.module.ts
+++ src/app/pages/camera/camera-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Camera1Component } from './camera1/camera1.component';
+import { Camera2Component } from './camera2/camera2.component';
 const routes: Routes = [
   {
     path: 'cam1',
     component: Camera1Component
+  },
+   {
+    path: 'cam2',
+    component: Camera2Component
   },
 ];
 
--- src/app/pages/camera/camera.module.ts
+++ src/app/pages/camera/camera.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { CameraRoutingModule } from './camera-routing.module';
 import { Camera1Component } from './camera1/camera1.component';
 import {WebcamModule} from 'ngx-webcam';
+import { Camera2Component } from './camera2/camera2.component';
 
 @NgModule({
   declarations: [
-    Camera1Component
+    Camera1Component,
+    Camera2Component
   ],
   imports: [
     CommonModule,
